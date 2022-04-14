b5e63a10b82b8974e1fcf956a2fcd0dfb5118420
Tudor Rusu <rusutudor@protonmail.com>
2022-04-14 13:09:54 +0300

    Created basic interface

---
+++ script.js
@@ -0,0 +1,4 @@
+const DISPLAY = document.querySelector('#display');
+const OPERATOR_BUTTONS = document.querySelectorAll('.operator-button');
+const NUMBER_BUTTONS = document.querySelectorAll('.number-button');
+
