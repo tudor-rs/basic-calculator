f3bd501a1b6e6abda96b61388519ebdca8c04f37
Tudor Rusu <rusutudor@protonmail.com>
2022-04-14 13:28:18 +0300

    Added functionality to display and buttons

--- script.js
+++ script.js
@@ -2,3 +2,17 @@ const DISPLAY = document.querySelector('#display');
 const OPERATOR_BUTTONS = document.querySelectorAll('.operator-button');
 const NUMBER_BUTTONS = document.querySelectorAll('.number-button');
 
+for (let i = 0; i < OPERATOR_BUTTONS.length; i++) {
+    OPERATOR_BUTTONS[i].addEventListener('click', () => {
+        DISPLAY.innerText += `${OPERATOR_BUTTONS[i].innerText}`;
+    });
+}
+
+for (let i = 0; i < NUMBER_BUTTONS.length; i++) {
+    NUMBER_BUTTONS[i].addEventListener('click', () => {
+        DISPLAY.innerText += `${NUMBER_BUTTONS[i].innerText}`;
+    });
+}
+
+
+
